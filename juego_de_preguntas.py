145218d5142e19aee413bc1325f49889fb7c5af7
creandotec <48784222+creandotec@users.noreply.github.com>
2019-04-27 23:17:35 -0500

    First working version

--- juego_de_preguntas.py
+++ juego_de_preguntas.py
@@ -1,9 +1,61 @@
 #!/usr/bin/env python3
 import xml.dom.minidom as xmlDom
-
+import pygame
+import sys
 class Preguntas:
     lista_de_preguntas = list()
+    answer = list()
 
+    answer.append("1")
+    user_chosen_option = "1"
+    def CheckAnswer(answer, numero_de_pregunta):
+        if Preguntas.answer[0] == Preguntas.user_chosen_option:
+            print("Ok")
+            return True
+        else:
+            return False
+
+class Display:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((0,0))
+        self.pregunta_background = 0
+        w,h = pygame.display.get_surface().get_size()
+        self.font = pygame.font.SysFont("comicsansms", 72)
+        self.text = self.font.render("Hello", True, (200, 200, 200))
+        self.width = w
+        self.height = h
+
+    def Load_new_image(self, numero_de_pregunta):
+        nombre_de_fondo = Preguntas.lista_de_preguntas[numero_de_pregunta].getElementsByTagName("Background")[0].getAttribute("file_name")
+        try:
+            self.pregunta_background = pygame.image.load(nombre_de_fondo)
+            self.pregunta_background = pygame.transform.scale(self.pregunta_background, (self.width, self.height))
+            print(nombre_de_fondo)
+
+        except pygame.error:
+            self.pregunta_background = pygame.image.load("./default.jpg")
+        self.Show_new_background()
+
+    def Show_new_background(self):
+        self.screen.fill((0,0,0))
+        self.screen.blit(self.pregunta_background, (0,0))
+        pygame.display.update()
+
+    def Show_chosen_answer(self, chosen_answer,numero_de_pregunta):
+        opciones = Preguntas.lista_de_preguntas[numero_de_pregunta].getElementsByTagName("Opcion")
+        texto = "no existe esa opción"
+        for opcion in opciones:
+            if opcion.getAttribute("tecla") == chosen_answer:
+                texto = opcion.getAttribute("texto")
+                print(texto)
+                print("Econtrado")
+                break
+            else:
+                print(opcion.getAttribute("tecla"))
+                print(chosen_answer)
+        self.text = self.font.render(texto, True, (0,0,0))
+        self.screen.blit(self.text, (100,100))
+        pygame.display.update()
 #Aquí configuramos las preguntas que se utilizarán durante el juego
 def get_questions():
     #Primero abrimos el documento XML en donde están las Preguntas
@@ -18,5 +70,35 @@ def get_questions():
     else:
         print("No está funcionando bien el script {}".format(collection.tagName))
 
+def main():
+    #Lleva el conteo de cuantas preguntas ya se mostraron en pantalla
+    numero_de_pregunta = 0
 
-get_questions()
+    pygame.init()
+    _display = Display()
+    done = False
+    clock = pygame.time.Clock()
+
+    _display.Load_new_image(numero_de_pregunta)
+    Done = False
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                key = pygame.key.name(event.key)
+                key = key.capitalize()
+                _display.Show_chosen_answer(key, numero_de_pregunta)
+
+                #En correcto se almacena si la respuesta elegida es correcta o no
+                correcto = Preguntas.CheckAnswer(key, numero_de_pregunta)
+                #Aumentamos en uno el número de pregunta para pasar a la siguiente
+                numero_de_pregunta += 1
+                #if correcto == True:
+        if numero_de_pregunta > len(Preguntas.lista_de_preguntas):
+            sys.exit()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    get_questions()
+    main()
